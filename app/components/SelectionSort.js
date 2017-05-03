a336ca53bf291974f23303029e308a2dd13c5f01
johnnny333 <janzarewicz@gmail.com>
2017-05-03 15:47:16 +0200

    Min value highlightning,iteration stopping

--- app/components/SelectionSort.js
+++ app/components/SelectionSort.js
@@ -7,41 +7,47 @@ export class SelectionSort extends React.Component {
 		let arrLength = 8;
 
 		super(props);
-		this.state = {
-			a: shuffle(Array.from({ length: arrLength }, (val, key) => key)),
-			i: arrLength - 1
-		};
+		this.state = {a: shuffle(Array.from({ length: arrLength }, (val, key) => key)), i: 0, minIndex: null };
 		this.sort = this.sort.bind(this);
 		this.handleChangeShuffle = this.handleChangeShuffle.bind(this);
 	}
 
 	handleChangeShuffle() {
-		this.setState({ a: shuffle(this.state.a) });
+		this.setState({ a: shuffle(this.state.a), i: 0, minIndex: null });
 	}
 
 	sort() {
 
-		var min, minIndex, temp, myTable = this.state.a;
+		var min, minIndex = this.state.minIndex, temp, myTable = this.state.a, i = this.state.i;
 
-		for (var i = 0; i < myTable.length - 1; i++) {
-			//szukaj elementu najmniejszego w nieposortowanej czesci tablicy
-			min = myTable[i];
-			minIndex = i;
-			for (var j = i; j < myTable.length; j++) {
-				if (myTable[j] < min) {
-					min = myTable[j];
-					minIndex = j;
-				}
-			}
-			//wstaw element najmniejszy na swoje miejsce
+
+		if (minIndex != null && minIndex < myTable.length) {
 			temp = myTable[i];
 			myTable[i] = myTable[minIndex];
 			myTable[minIndex] = temp;
+
+			this.setState({ a: myTable, i: this.state.i + 1, minIndex: null });
+			return;
 		}
-		this.setState({a:myTable});
+
+		min = myTable[i], minIndex = i;
+
+		for (var j = i; j < myTable.length; j++) {
+			if (myTable[j] < min) {
+				min = myTable[j];
+				minIndex = j;
+				console.log("minIndex: " + minIndex);
+			}
+		}
+		this.setState({ minIndex: minIndex });
+		return;
 	}
 
 	render() {
+
+		let currentIter = this.state.i, minIndex = this.state.minIndex,
+			disabled = this.state.minIndex == this.state.a.length ? true : false;
+		console.log("minIndex: " + this.state.minIndex);
 
 		return (
 			<div>
@@ -49,12 +55,14 @@ export class SelectionSort extends React.Component {
 			<button onClick={this.handleChangeShuffle}>Shuffle</button>
 
 				<form onSubmit={e => (e.preventDefault())}>
-					<button onClick={this.sort} disabled={this.state.disabled}>Sort</button>
+					<button onClick={this.sort} disabled={disabled}>Sort</button>
 				</form>
 
 			{ /* Render spans representing array elements */ }
 			{this.state.a.map(function(object, i) {
-
+				if(i == minIndex){return <span className="found" key={i}>{object}</span>;}
+				if(i <= currentIter){return <span className="selected" key={i}>{object}</span>;}
+				
 				return <span key={i} >{object}</span>;
 			})
 			}
