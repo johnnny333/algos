ca94aeb70cac0dd298be3b10cba930ecdf748d21
johnnny333 <janzarewicz@gmail.com>
2017-05-08 00:02:23 +0200

    proper a and b values display

--- app/components/arithmetic_algos/EuclidianAlgorithm.js
+++ app/components/arithmetic_algos/EuclidianAlgorithm.js
@@ -4,31 +4,45 @@ export class EuclidianAlgorithm extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = { a: 1112, b: 695, r: null, spans: [] };
+		this.state = { a: 1112, b: 695, spans: [] };
 		this.handleChangeInputA = this.handleChangeInputA.bind(this);
 		this.handleChangeInputB = this.handleChangeInputB.bind(this);
 		this.gcd = this.gcd.bind(this);
+		this.clean = this.clean.bind(this);
 	}
 
 	handleChangeInputA(event) {
 		this.setState({ a: event.target.value });
+		this.clean();
 	}
 
 	handleChangeInputB(event) {
 		this.setState({ b: event.target.value });
+		this.clean();
+	}
+
+	clean(){
+		this.setState({spans: []} );
 	}
 
 	gcd(){
 
 		let r, a = this.state.a, b = this.state.b;
 
+		//Display a and b values on first iteration
+		if(!this.state.spans[0]){
+			this.state.spans.push({a: a, b: b, r: a % b});
+			this.setState({a: a, b: b});
+			return;
+		}
+
+		//Actual GCD calculation
 		if (b != 0) {
 			r = a % b;
 			a = b;
 			b = r;
 
 			this.state.spans.push({a: a, b: b, r: a % b});
-
 			this.setState({a: a, b: b});
 		}
 	}
