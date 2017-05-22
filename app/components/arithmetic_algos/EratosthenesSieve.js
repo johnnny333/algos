416a4a74e5fbef601742d03552d64a92814fcc18
johnnny333 <janzarewicz@gmail.com>
2017-05-22 02:33:14 +0200

    Iterative MergeSort

--- app/components/arithmetic_algos/EratosthenesSieve.js
+++ app/components/arithmetic_algos/EratosthenesSieve.js
@@ -10,46 +10,48 @@ export class EratosthenesSieve extends React.Component {
 	}
 
 	handleChange(event) {
-		this.setState({n: event.target.value, isPrime: [], factor: 2});
+		this.setState({ n: event.target.value, isPrime: [], factor: 2 });
 	}
 
 	sieve() {
 		//http://introcs.cs.princeton.edu/java/14array/PrimeSieve.java.html
 
-		let n = this.state.n, isPrime = this.state.isPrime, factor = this.state.factor;
+		let n = this.state.n,
+			isPrime = this.state.isPrime,
+			factor = this.state.factor;
 
-        // initially assume all integers are prime
-        if(!isPrime[2]){
-    		for (let i = 2; i <= n; i++) {
-            	isPrime[i] = {isPrime: true, i: i};
-        	}
-        	this.setState({isPrime: isPrime});
-        	return;
-    	}
+		// initially assume all integers are prime
+		if (!isPrime[2]) {
+			for (let i = 2; i <= n; i++) {
+				isPrime[i] = { isPrime: true, i: i };
+			}
+			this.setState({ isPrime: isPrime });
+			return;
+		}
 
-        // mark non-primes <= n using Sieve of Eratosthenes
-        if ( factor*factor <= n) {
+		// mark non-primes <= n using Sieve of Eratosthenes
+		if (factor * factor <= n) {
 
-            // if factor is prime, then mark multiples of factor as nonprime
-            // suffices to consider mutiples factor, factor+1, ...,  n/factor
-            if (isPrime[factor].isPrime) {
-                for (let j = factor; factor*j <= n; j++) {
-                    isPrime[factor*j].isPrime = false;
-                }
-            }
-            this.setState({factor: this.state.factor + 1})
-        } else {
-        	this.setState({disabled: true});
-        	return;
-        }
+			// if factor is prime, then mark multiples of factor as nonprime
+			// suffices to consider mutiples factor, factor+1, ...,  n/factor
+			if (isPrime[factor].isPrime) {
+				for (let j = factor; factor * j <= n; j++) {
+					isPrime[factor * j].isPrime = false;
+				}
+			}
+			this.setState({ factor: this.state.factor + 1 })
+		} else {
+			this.setState({ disabled: true });
+			return;
+		}
 
-        // count primes
-        let primes = 0;
-        for (let i = 2; i <= n; i++) {
-            if (isPrime[i].isPrime) primes++;
-        }
+		// count primes
+		let primes = 0;
+		for (let i = 2; i <= n; i++) {
+			if (isPrime[i].isPrime) primes++;
+		}
 
-        this.setState({isPrime: isPrime});
+		this.setState({ isPrime: isPrime });
 	}
 
 	render() {
@@ -72,11 +74,12 @@ export class EratosthenesSieve extends React.Component {
 
 			{this.state.isPrime.map(function(object, i){
 
-					if(object.isPrime) {return <span key={i} className="found">{object.i}</span>}
-					else {return <span key={i} >{object.i}</span>}
-				})}
+				if(object.isPrime) {
+					return <span key={i} className="found">{object.i}</span>;	
+				} else {return <span key={i} >{object.i}</span>}
+			})}
 
 			</div>
-			)
+		);
 	}
 }
--- app/components/sorting/MergeSort.js
+++ app/components/sorting/MergeSort.js
@@ -1,8 +1,78 @@
 import React from "react";
+import { shuffle } from "../../helpers/HelperFunctions";
 
 export class MergeSort extends React.Component {
-  
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+
+	constructor(props) {
+		let arrLength = 8;
+		super(props);
+		this.state = { a: shuffle(Array.from({ length: arrLength }, (val, key) => key)) };
+		this.handleChangeShuffle = this.handleChangeShuffle.bind(this);
+		this.sort = this.sort.bind(this);
+		this.merge = this.merge.bind(this);
+	}
+
+	// Shuffle an array and reset component to its initial state
+	handleChangeShuffle() {
+		this.setState({ a: shuffle(this.state.a) });
+	}
+
+	sort(items) {
+
+		if (items.length == 1) {
+			return items;
+		}
+
+		var work = [];
+		for (var i = 0, len = items.length; i < len; i++) {
+			work.push([items[i]]);
+		}
+		work.push([]); //in case of odd number of items
+
+		for (var lim = len; lim > 1; lim = Math.floor((lim + 1) / 2)) {
+			for (var j = 0, k = 0; k < lim; j++, k += 2) {
+				work[j] = this.merge(work[k], work[k + 1]);
+			}
+			work[j] = []; //in case of odd number of items
+		}
+		
+		console.log("array: " + work[0])
+		return work[0];
+
+	}
+
+	merge(left, right) {
+		var result = [];
+
+		while (left.length > 0 && right.length > 0) {
+			if (left[0] < right[0]) {
+				result.push(left.shift());
+			} else {
+				result.push(right.shift());
+			}
+		}
+
+		return result.concat(left).concat(right);
+	}
+
+	render() {
+
+		return (
+
+			<div>
+			<button onClick={this.handleChangeShuffle}>Shuffle</button>
+
+				<form onSubmit={e => (e.preventDefault())}>
+					<button onClick={() => this.sort(this.state.a)} >Sort</button>
+				</form>
+
+			{ /* Render spans representing array elements */ }
+			{this.state.a.map(function(object, i) {
+
+				return <span key={i} >{object}</span>;
+			})
+			}
+			</div>
+		);
+	}
 }
