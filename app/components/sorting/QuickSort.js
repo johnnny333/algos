49beecfef0cfb204d2d755d9ed53a41a162ff936
johnnny333 <janzarewicz@gmail.com>
2017-05-31 17:59:19 +0200

    QuickSort iterative...

--- app/components/sorting/QuickSort.js
+++ app/components/sorting/QuickSort.js
@@ -7,9 +7,9 @@ export class QuickSort extends React.Component {
 		let arrLength = 8;
 
 		super(props);
-		this.state = { a: shuffle(Array.from({ length: arrLength }, (val, key) => key)) };
+		this.state = { a: shuffle(Array.from({ length: arrLength }, (val, key) => key)), stack: [], sorted: [],
+			pivot: null };
 		this.quickSort = this.quickSort.bind(this);
-		this.partition = this.partition.bind(this);
 		this.handleChangeShuffle = this.handleChangeShuffle.bind(this);
 	}
 
@@ -17,61 +17,80 @@ export class QuickSort extends React.Component {
 		this.setState({ a: shuffle(this.state.a), i: 0, j: null });
 	}
 
-	quickSort(tablica, p, r) {
-
-		let q;
-		if (p < r) {
-			q = this.partition(tablica, p, r); // dzielimy tablice na dwie czesci; q oznacza punkt podzialu
-			this.quickSort(tablica, p, q); // wywolujemy rekurencyjnie quicksort dla pierwszej czesci tablicy
-			this.quickSort(tablica, q + 1, r); // wywolujemy rekurencyjnie quicksort dla drugiej czesci tablicy
-		}
+	quickSort() {
 		
-		this.setState({ a: tablica });
-	}
+		let stack = this.state.stack, sorted = this.state.sorted, pivot = this.state.pivot;
 
-	// dzielimy tablice na dwie czesci, w pierwszej wszystkie liczby sa mniejsze badz rowne x, w drugiej wieksze lub rowne od x
-	partition(tablica, p, r) {
-		let x = tablica[p]; // obieramy x
-		let i = p,
-			j = r,
-			w; // i, j - indeksy w tabeli
-
-		// petla nieskonczona - wychodzimy z niej tylko przez return j
-		while (true) {
-			while (tablica[j] > x) // dopoki elementy sa wieksze od x
-				j--;
-
-			while (tablica[i] < x) // dopoki elementy sa mniejsze od x
-				i++;
-
-			// zamieniamy miejscami gdy i < j
-			if (i < j) {
-				w = tablica[i];
-
-				tablica[i] = tablica[j];
-				tablica[j] = w;
-				i++;
-				j--;
-			} else // gdy i >= j zwracamy j jako punkt podzialu tablicy
-				return j;
+		if(stack[0] == null){
+			stack = [this.state.a];
 		}
+
+		if (stack.length) {
+ 
+			var temp = stack.pop(), tl = temp.length;
+	
+			if (tl == 1) {
+				sorted.push(temp[0]);
+				// continue;
+				return;
+			}
+			pivot = temp[0];
+			var left = [], right = [];
+	
+			for (var i = 1; i < tl; i++) {
+				if (temp[i] < pivot) {
+					left.push(temp[i]);
+				} else {
+					right.push(temp[i]);
+				}
+			}
+	
+			left.push(pivot);
+	
+			if (right.length)
+				stack.push(right);
+			if (left.length)
+				stack.push(left);
+
+			this.setState({stack: stack, sorted: sorted, pivot: pivot});
+	
+		}
+ 
+		console.log(sorted);
+		// this.setState({a: sorted});
 	}
+
 
 	render() {
+
+		let pivot = this.state.pivot;
+
 		return (
 
 			<div>
 			<button onClick={this.handleChangeShuffle}>Shuffle</button>
 
 				<form onSubmit={e => (e.preventDefault())}>
-					<button onClick={() => this.quickSort(this.state.a, 0, this.state.a.length -1 )} >QuickSort</button>
+					<button onClick={this.quickSort } >QuickSort</button>
 				</form>
 
 			{ /* Render spans representing array elements */ }
 			{this.state.a.map(function(object, i) {
+
+				if(i == pivot){return <span className="found" key={i}>{object}</span>;}
+
 				return <span key={i} >{object}</span>;
 			})
 			}
+
+			<div></div>
+
+			{this.state.sorted.map(function(object, i) {
+
+				return <span key={i} >{object}</span>;
+			})
+			}	
+
 			</div>
 		);
 	}
