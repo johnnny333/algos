3d4b05a0e1e18e9b636f76d799e79110d7532168
johnnny333 <janzarewicz@gmail.com>
2017-06-06 13:20:20 +0200

    QuickSort moving

--- app/components/sorting/QuickSort.js
+++ app/components/sorting/QuickSort.js
@@ -4,39 +4,57 @@ import { shuffle } from "../../helpers/HelperFunctions";
 export class QuickSort extends React.Component {
 
 	constructor(props) {
-		let arrLength = 8;
+		let arrLength = 9;
 
 		super(props);
-		this.state = { a: shuffle(Array.from({ length: arrLength }, (val, key) => key)), stack: [], sorted: [],
-			pivot: null };
+		this.state = {
+			a: shuffle(Array.from({ length: arrLength }, (val, key) => key)),
+			stack: [],
+			sorted: [],
+			pivot: null
+		};
 		this.quickSort = this.quickSort.bind(this);
 		this.handleChangeShuffle = this.handleChangeShuffle.bind(this);
 	}
 
 	handleChangeShuffle() {
-		this.setState({ a: shuffle(this.state.a), i: 0, j: null });
+		this.setState({ a: shuffle(this.state.a), stack: [], sorted: [], pivot: null});
 	}
 
 	quickSort() {
-		
-		let stack = this.state.stack, sorted = this.state.sorted, pivot = this.state.pivot;
 
-		if(stack[0] == null){
+		let stack = this.state.stack,
+			sorted = this.state.sorted,
+			pivot = this.state.pivot;
+
+		if (stack[0] == null) {
 			stack = [this.state.a];
 		}
 
-		if (stack.length && sorted.length <= this.state.a.length) {
- 
-			var temp = stack.pop(), tl = temp.length;
-	
+		if (stack.length ) {
+
+			var temp = stack.pop(),
+				tl = temp.length;
+
 			if (tl == 1) {
 				sorted.push(temp[0]);
 				// continue;
 				return;
 			}
 			pivot = temp[0];
-			var left = [], right = [];
-	
+			var left = [],
+				right = [];
+
+			//Flat an stack array
+			let merged = [].concat.apply([], stack);
+
+			let rest = temp.concat(merged);
+
+			rest = sorted.concat(rest);
+
+			console.log(sorted);
+			console.log(rest);
+
 			for (var i = 1; i < tl; i++) {
 				if (temp[i] < pivot) {
 					left.push(temp[i]);
@@ -44,30 +62,29 @@ export class QuickSort extends React.Component {
 					right.push(temp[i]);
 				}
 			}
-	
+
 			left.push(pivot);
-	
+
 			if (right.length)
 				stack.push(right);
 			if (left.length)
 				stack.push(left);
 
-			// console.log(left);
-			// console.log(right);
-			console.log(stack[1].concat(stack[0]))
+			if(rest.length <= this.state.a.length){
+				this.setState({ stack: stack, pivot: pivot , a: rest });
+			}
 
-			this.setState({stack: stack, sorted: sorted, pivot: pivot});
-			Array.prototype.splice.apply(this.state.a, [0, sorted.length].concat(sorted));
-	
+			//Put right side array starting from pivot position
+			// Array.prototype.splice.apply(this.state.a, [sorted.length, rest.length].concat(rest));
 		}
- 
-		// console.log(sorted);
+		console.log(sorted);
+		this.setState({ sorted: sorted});
 	}
-
 
 	render() {
 
-		let pivot = this.state.pivot, sortedLenght = this.state.sorted.length;
+		let pivot = this.state.pivot,
+			sortedLenght = this.state.sorted.length;
 
 		return (
 
@@ -81,8 +98,8 @@ export class QuickSort extends React.Component {
 			{ /* Render spans representing array elements */ }
 			{this.state.a.map(function(object, i) {
 
-				if(i == pivot){return <span className="found" key={i}>{object}</span>;}
 				if(i < sortedLenght){return <span className="selected" key={i}>{object}</span>;}
+				if(i == pivot){return <span className="found" key={i}>{object}</span>;}
 
 				return <span key={i} >{object}</span>;
 			})
